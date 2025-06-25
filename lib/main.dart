9b01a04c9e0583707b19960ba55a62b575167abf
Ange-Constance <constance.nimuhire@gmail.com>
2025-06-25 18:07:48 +0200

    Initialize Flutter app with Material dark theme and main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,5 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     );
   }
-  class ParticleDemo extends StatelessWidget {
-  const ParticleDemo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return Scaffold(
-      body: Stack(
-        children: [
-       
-          Center(
-            child: Text(
-              'Welcome to My App!',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Colors.white.withOpacity(0.9),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+  
 }
